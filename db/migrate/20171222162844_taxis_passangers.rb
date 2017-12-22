334733524d0e1495504e44cd187c209a3c5ddf51
Justin Tooley <jstooley823@gmail.com>
2017-12-22 16:29:45 +0000

    automatically backed up by learn

--- db/migrate/20171222162844_taxis_passangers.rb
+++
@@ -1,4 +0,0 @@
-class TaxisPassangers < ActiveRecord::Migration
-  def change
-  end
-end
---
+++ db/migrate/20171222162945_create_taxis_passangers.rb
@@ -0,0 +1,6 @@
+class CreateTaxisPassangers < ActiveRecord::Migration
+  def change
+    create_table :taxis_passangers do |t|
+    end
+  end
+end
