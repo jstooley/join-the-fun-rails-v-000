da2249f794161cd8304db1390f3a5d8de5735bc3
Justin Tooley <jstooley823@gmail.com>
2017-12-22 16:18:48 +0000

    automatically backed up by learn

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,2 +1,4 @@
 class Passenger < ActiveRecord::Base
+  has_many :taxis_passangers
+  has_many :taxis, through: :taxis_passangers
 end
--- app/models/taxi.rb
+++ app/models/taxi.rb
@@ -1,2 +1,4 @@
 class Taxi < ActiveRecord::Base
+  has_many :taxis_passangers
+  has_many :taxis, through: :taxis_passangers
 end
